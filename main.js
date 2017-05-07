53ed3296fca817073d0cd954e56bf2f8bf3f9f88
Quentin Renard <contact@asticode.com>
2017-05-07 15:07:23 +0200

    Added menu

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
 'use strict'
 
 const electron = require('electron')
-const {app, BrowserWindow, ipcMain} = electron
+const {app, BrowserWindow, ipcMain, Menu, MenuItem} = electron
 const consts = require('./src/consts.js')
 const client = require('./src/client.js').init()
 const rl = require('readline').createInterface({input: client.socket})
 
 let elements = {}
+let menus = {}
 
 // App is ready
 app.on('ready',() => {
     // Init
     const screen = electron.screen
-    
-    // Send electron.ready event
-    client.write(consts.mainTargetID, consts.eventNames.appEventReady, {displays: {all: screen.getAllDisplays(), primary: screen.getPrimaryDisplay()}})
 
     // Listen to screen events
     screen.on('display-added', function() {
@@ -39,6 +37,61 @@ app.on('ready',() => {
 
         // Switch on event name
         switch (json.name) {
+            // Menu
+            case consts.eventNames.menuCmdCreate:
+            menus[json.menu.rootId] = menuCreate(json.menu)
+            setMenu(json.menu.rootId)
+            client.write(json.targetID, consts.eventNames.menuEventCreated)
+            break;
+
+            // Menu item
+            case consts.eventNames.menuItemCmdSetChecked:
+            elements[json.targetID].checked = json.menuItemOptions.checked
+            client.write(json.targetID, consts.eventNames.menuItemEventCheckedSet)
+            break;
+            case consts.eventNames.menuItemCmdSetEnabled:
+            elements[json.targetID].enabled = json.menuItemOptions.enabled
+            client.write(json.targetID, consts.eventNames.menuItemEventEnabledSet)
+            break;
+            case consts.eventNames.menuItemCmdSetLabel:
+            elements[json.targetID].label = json.menuItemOptions.label
+            client.write(json.targetID, consts.eventNames.menuItemEventLabelSet)
+            break;
+            case consts.eventNames.menuItemCmdSetVisible:
+            elements[json.targetID].visible = json.menuItemOptions.visible
+            client.write(json.targetID, consts.eventNames.menuItemEventVisibleSet)
+            break;
+
+            // Sub menu
+            case consts.eventNames.subMenuCmdAppend:
+            elements[json.targetID].append(menuItemCreate(json.menuItem))
+            setMenu(json.menuItem.rootId)
+            client.write(json.targetID, consts.eventNames.subMenuEventAppended)
+            break;
+            case consts.eventNames.subMenuCmdClosePopup:
+            var window = null
+            if (typeof json.windowId !== "undefined") {
+                window = elements[json.windowId]
+            }
+            elements[json.targetID].closePopup(window)
+            client.write(json.targetID, consts.eventNames.subMenuEventClosedPopup)
+            break;
+            case consts.eventNames.subMenuCmdInsert:
+            elements[json.targetID].insert(json.menuItemPosition, menuItemCreate(json.menuItem))
+            setMenu(json.menuItem.rootId)
+            client.write(json.targetID, consts.eventNames.subMenuEventInserted)
+            break;
+            case consts.eventNames.subMenuCmdPopup:
+            var window = null
+            if (typeof json.windowId !== "undefined") {
+                window = elements[json.windowId]
+            }
+            json.menuPopupOptions.async = true
+            elements[json.targetID].popup(window, json.menuPopupOptions)
+            client.write(json.targetID, consts.eventNames.subMenuEventPoppedUp)
+            break;
+
+            // Window
             case consts.eventNames.windowCmdBlur:
             elements[json.targetID].blur()
             break;
@@ -92,7 +145,51 @@ app.on('ready',() => {
             break;
         }
     })
+
+    // Send electron.ready event
+    client.write(consts.mainTargetID, consts.eventNames.appEventReady, {displays: {all: screen.getAllDisplays(), primary: screen.getPrimaryDisplay()}})
 })
+
+// menuCreate creates a new menu
+function menuCreate(menu) {
+    if (typeof menu !== "undefined") {
+        elements[menu.id] = new Menu()
+        for(var i = 0; i < menu.items.length; i++) {
+            elements[menu.id].append(menuItemCreate(menu.items[i]))
+        }
+        return elements[menu.id]
+    }
+    return null
+}
+
+// menuItemCreate creates a menu item
+function menuItemCreate(menuItem) {
+    const itemId = menuItem.id
+    menuItem.options.click = function(menuItem) {
+        client.write(itemId, consts.eventNames.menuItemEventClicked, {menuItemOptions: menuItemToJSON(menuItem)})
+    }
+    if (typeof menuItem.submenu !== "undefined") {
+        menuItem.options.type = 'submenu'
+        menuItem.options.submenu = menuCreate(menuItem.submenu)
+    }
+    elements[itemId] = new MenuItem(menuItem.options)
+    return elements[itemId]
+}
+
+// menuItemToJSON returns the proper fields not to raise an exception
+function menuItemToJSON(menuItem) {
+    return {
+        checked: menuItem.checked,
+        enabled: menuItem.enabled,
+        label: menuItem.label,
+        visible: menuItem.visible,
+    }
+}
+
+// setMenu sets a menu
+function setMenu(rootId) {
+    rootId == consts.mainTargetID ? Menu.setApplicationMenu(menus[rootId]) : elements[json.menu.parentId].setMenu(menus[rootId])
+}
 
 // windowCreate creates a new window
 function windowCreate(json) {
