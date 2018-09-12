bfebaf0ccf7c47f71f67d9a30d0bb94793281249
cbairy <chandrakanth.bairy@gmail.com>
2018-09-12 11:07:25 +0530

    Merge from single-instance-app to master

--- main.js
+++ main.js
@@ -518,11 +518,7 @@ function windowCreateFinish(json) {
             url: url
         })
     })
-<<<<<<< HEAD
-    myWindow = elements[json.targetID];
-=======
     myWindow = elements[json.targetID]
->>>>>>> master
 }
 
 function registerCallback(json, k, e, n, c) {
