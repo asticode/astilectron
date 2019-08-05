5c97ad3d818ed104e1c6e66d0567a9f19327192e
Unknown <foxtrotalphalima@gmail.com>
2019-08-05 17:58:50 -0400

    perf(index.js): Removed unneeded code.
    Removed code is duplicated in `cli.js`.

--- index.js
+++ index.js
@@ -15,35 +15,7 @@ let menus = {};
 let quittingApp = false;
 
 // Single instance
-export let lastWindow = null;
-if (process.argv[3] === "true") {
-    // Lock
-    const singlesInstanceLock = app.requestSingleInstanceLock();
-    if (!singlesInstanceLock) {
-        app.quit();
-        return;
-    }
-
-    // Someone tried to run a second instance, we should focus our window.
-    app.on('second-instance', (event, commandLine, workingDirectory) => {
-        if (lastWindow) {
-            if (lastWindow.isMinimized()) lastWindow.restore();
-            lastWindow.show();
-        }
-    });
-}
-
-// Command line switches
-let idx = 4;
-for (let i = idx; i < process.argv.length; i++) {
-    let s = process.argv[i].replace(/^[\-]+/g,"");
-    let v;
-    if (typeof process.argv[i+1] !== "undefined" && !process.argv[i+1].startsWith("-")) {
-        v = process.argv[i+1];
-        i++;
-    }
-    app.commandLine.appendSwitch(s, v);
-}
+let lastWindow = null;
 
 // App is quitting
 const beforeQuit = () => quittingApp = true;
